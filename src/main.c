3e77fe86bbbfdf7088d45f655a81ab8d4940f2bf
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-06-15 18:05:55 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "tokenizer.h"
 
 const int MAX_INPUT_LENGTH = 65536;
 char *input_buffer;
 Tokens tk;
+const char *valid_commands[] = {"exit", "echo", "type"};
+const int valid_command_count = sizeof(valid_commands) / sizeof(valid_commands[0]);
 
 void free_tk(Tokens *tk) {
 	if (tk->tokens != NULL) {
@@ -41,6 +44,21 @@ void echo(Tokens tk) {
 	printf("\n");
 }
 
+void type(Tokens tk) {
+	bool found;
+	for (int i = 1; i < tk.num_tokens; i++) {
+		found = false;
+		for (int j = 0; j < valid_command_count; j++) {
+			if (strcmp(valid_commands[j], tk.tokens[i]) == 0) {
+				found = true;
+				break;
+			}
+		}
+		if (found) printf("%s is a shell builtin\n", tk.tokens[i]);
+		else printf("%s: not found\n", tk.tokens[i]);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	atexit(cleanup_heap);
 	setbuf(stdout, NULL);
@@ -51,7 +69,7 @@ int main(int argc, char *argv[]) {
 	char *temp_inp;
 	Tokens temp_tk;
 
-	while (1) {
+	while (true) {
 		printf("$ ");
 	
 		temp_inp = read_input(input_buffer, MAX_INPUT_LENGTH);
@@ -63,8 +81,9 @@ int main(int argc, char *argv[]) {
 		free_tk(&tk);
 		tk = temp_tk;
 
-		if (strcmp(tk.tokens[0], "exit") == 0) return 0;
-		if (strcmp(tk.tokens[0], "echo") == 0) echo(tk);
+		if      (strcmp(tk.tokens[0], "exit") == 0) return 0;
+		else if (strcmp(tk.tokens[0], "echo") == 0) echo(tk);
+		else if (strcmp(tk.tokens[0], "type") == 0) type(tk);
 	
 		else printf("%s: command not found\n", input_buffer);
 	}
