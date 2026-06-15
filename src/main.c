e4b1d50f01d6e9650745edce74d44013c3971e3a
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-06-15 15:46:00 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* read_input() {
+	const int MAX_LENGTH = 65536;
+
+	char *buffer = malloc(MAX_LENGTH);
+	if (buffer == NULL) return NULL;
+
+	if (fgets(buffer, MAX_LENGTH, stdin) != NULL) {
+        buffer[strcspn(buffer, "\n")] = '\0';
+    }
+
+	return buffer;
+}
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+	// Flush after every printf
+	setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+	printf("$ ");
 
-  return 0;
+	char *input = read_input();
+	if (input == NULL) return 1;
+
+	printf("%s: command not found\n", input);
+
+	return 0;
 }
